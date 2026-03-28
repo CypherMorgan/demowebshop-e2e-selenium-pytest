9f75217195c3b3e03728322891b6389875b9bce6
I am Cypher <108661795+CypherMorgan@users.noreply.github.com>
2026-03-28 07:28:42 -0700

    Update search_page.py

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 
@@ -7,4 +8,17 @@ class SearchPage(BasePage):
     PRODUCT_LINK = (By.CSS_SELECTOR, ".product-title a")
 
     def open_first_product(self):
-        self.click(self.PRODUCT_LINK)
+
+        self.wait.until(EC.presence_of_element_located(self.PRODUCT_LINK))
+
+        for _ in range(2):
+            try:
+                element = self.wait.until(
+                    EC.element_to_be_clickable(self.PRODUCT_LINK)
+                )
+                element.click()
+                return
+            except Exception:
+                continue
+
+        raise Exception("Failed to click product (stale element)")
