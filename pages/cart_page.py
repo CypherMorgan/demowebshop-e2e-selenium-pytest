4608c09035f853f0606a87017fd1061c2cc2c263
I am Cypher <108661795+CypherMorgan@users.noreply.github.com>
2026-03-28 07:55:07 -0700

    Update cart_page.py

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,13 +1,19 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 
 class CartPage(BasePage):
 
     TERMS_CHECKBOX = (By.ID, "termsofservice")
-    CHECKOUT_BTN = (By.ID, "checkout")
+    CHECKOUT_BUTTON = (By.ID, "checkout")
 
     def checkout(self):
 
-        self.click(self.TERMS_CHECKBOX)
-        self.click(self.CHECKOUT_BTN)
+        self.wait.until(EC.presence_of_element_located(self.TERMS_CHECKBOX))
+
+        self.wait.until(EC.url_contains("cart"))
+
+        self.wait.until(EC.element_to_be_clickable(self.TERMS_CHECKBOX)).click()
+
+        self.wait.until(EC.element_to_be_clickable(self.CHECKOUT_BUTTON)).click()
